3dff4e04dd438c2edeb987c1a07e684513eb25a6
BKluuis <luisscalabrin@gmail.com>
2025-06-03 20:00:03 -0300

    feat: adicionado PrimeNG themes e configurado tema Aura

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,4 @@
+import Aura from '@primeng/themes/aura';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -5,10 +6,15 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-// import { Aura } from 'primeng/theme/aura'; // Removed: theme presets are referenced by string name
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimationsAsync(), providePrimeNG({
-    theme: { preset: 'aura' }
-  })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: { preset: Aura }
+    })
+  ]
 };
